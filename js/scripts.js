ce52758454c464b6f499c6075db999c1741e3fa0
SkyeN <skye@dames.es>
2018-08-15 14:41:06 -0700

    add input field for base

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-var numberCheck = function (binary) {
+var numberCheck = function (binary,base) {
   var numberStr = binary.toString();
-  if (!numberStr.match(/^[01]+$/)) {
+  if (!numberStr.match(/^[01]+$/) && base.isNumeric) {
     alert("Please enter a binary number");
   }
   else return conversion(binary);
 }
 
 
-var conversion = function(binary) {
+var conversionBtoD = function(binary) {
   var result = 0;
   var reversedArr = binary.toString().split("").reverse();
   for (var i=0; i<reversedArr.length;i++) {
@@ -16,11 +16,16 @@ var conversion = function(binary) {
   return result;
 }
 
+var conversionDtoB = function(decimal) {
+  var result = 0;
+  
+}
 $(document).ready(function(){
   $("#input").submit(function(event){
     event.preventDefault();
     var inputNumber = parseInt($("#inputNumber").val());
+    var inputBase = parseInt($("#inputBase").val());
     $("#result").empty();
-    $("#result").append(numberCheck(inputNumber));
+    $("#result").append(numberCheck(inputNumber,inputBase));
   })
 });
