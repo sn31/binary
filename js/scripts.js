d82530ff906acb2c77ebd47c3e409f9fed22fd33
SkyeN <skye@dames.es>
2018-08-15 15:11:34 -0700

    add backend logic to conver decimal to base n

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,42 @@
-var numberCheck = function (binary,base) {
+var numberCheck = function (binary, base) {
   var numberStr = binary.toString();
   if (!numberStr.match(/^[01]+$/) && base.isNumeric) {
     alert("Please enter a binary number");
   }
-  else return conversion(binary);
+  else return conversionBtoD(binary);
 }
 
 
-var conversionBtoD = function(binary) {
+var conversionBtoD = function (binary) {
   var result = 0;
   var reversedArr = binary.toString().split("").reverse();
-  for (var i=0; i<reversedArr.length;i++) {
-    result += reversedArr[i]*Math.pow(2,i);
+  for (var i = 0; i < reversedArr.length; i++) {
+    result += reversedArr[i] * Math.pow(2, i);
   }
   return result;
 }
 
-var conversionDtoB = function(decimal) {
+var conversionDtoB = function (decimal, base) {
   var result = 0;
+  return result = decimal.toString(base);
   
 }
-$(document).ready(function(){
-  $("#input").submit(function(event){
+
+$(document).ready(function () {
+  $("#input").submit(function (event) {
     event.preventDefault();
     var inputNumber = parseInt($("#inputNumber").val());
     var inputBase = parseInt($("#inputBase").val());
-    $("#result").empty();
-    $("#result").append(numberCheck(inputNumber,inputBase));
+    if (inputBase !== "") {
+      $("#result").empty();
+      $("#result").append(conversionDtoB(numberCheck(inputNumber, inputBase), inputBase));
+      console.log(numberCheck(inputNumber, inputBase));
+      console.log(conversionDtoB(numberCheck(inputNumber, inputBase), inputBase));
+    }
+    else {
+      $("#result").empty();
+      $("#result").append(numberCheck(inputNumber, inputBase));
+    }
+
   })
 });
